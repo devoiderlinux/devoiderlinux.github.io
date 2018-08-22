f7c8933910af942ae1a42a8d3d2d44c2a8073b87
Le Devoid Linux <devoiderlinux@linuxmail.org>
2018-08-21 21:43:59 -0500

    New

---
+++ index.php
@@ -0,0 +1,19 @@
+<html> 
+    
+    <head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+
+
+    <body>
+        <center><img src="devoid.gif"></center>
+        <title> Devoid Linux, a GNU/Linux distro on Void GNU/Linux </title>
+        <h1> Devoid Linux, a GNU/Linux distro on Void GNU/Linux </h1>
+        <h2> Use the links below for reference or, download <a href="download.php"> here</a>.</h2>
+        <a href="install.html"> Installation </a>
+        <br>
+        <a href="about.html">About</a>
+    </body>
+
+</html>
